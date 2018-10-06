6c2a10d7dd31232b1c03cf9142143ff993b8595e
Johnny Liang <zhonglin.liang@sjsu.edu>
2018-10-06 16:47:14 -0700

    fix jsp config, add more api calls

--- src/main/java/twitter/ApiMain.java
+++ src/main/java/twitter/ApiMain.java
@@ -1,3 +1,4 @@
+package twitter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,7 +27,7 @@ public class ApiMain extends HttpServlet{
 	public static void main(String []args) {
 		ApiMain api = new ApiMain();
 		api.application_only_auth();
-		apiSearch("league%20of%20legends","popular");
+		apiGetFollower("lolesports");
 	}
 	
 	public static void makeConnection(String urlString) {
@@ -40,6 +41,7 @@ public class ApiMain extends HttpServlet{
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
 		}
 		
 	}
@@ -83,14 +85,12 @@ public class ApiMain extends HttpServlet{
 			e.printStackTrace();
 		} 
 	}
-	public static JSONObject apiSearch(String keywords, String result_type) { // there are more parameters available, for now just two
+	public static String apiSearch(String keywords, String result_type) { // there are more parameters available, for now just two
 		try {
 			makeConnection("https://api.twitter.com/1.1/search/tweets.json?q=" + keywords + "&" + "result_type=" + result_type); 
 			System.out.println(connection.getURL().toString());
 			connection.setRequestMethod("GET");
-			String bearer = "Bearer" + " " + bearer_key;
-			System.out.println(bearer);
-			connection.setRequestProperty("Authorization",bearer);
+			connection.setRequestProperty("Authorization","Bearer" + " " + bearer_key);
 			connection.connect();
 			if(connection.getResponseMessage().equals("OK")) {
 				
@@ -98,11 +98,12 @@ public class ApiMain extends HttpServlet{
 				
 				// a demonstration of processing json data, should be inside a feature function
 				JSONArray array = json.getJSONArray("statuses"); // there are multiple tweets under statuses tab
+				String output = "";
 				for(int i=0;i<array.length();i++) { // loop through all the tweets, print the text string if available
-					System.out.println(array.getJSONObject(i).optString("text")+"\n");
-					
+					output = output + array.getJSONObject(i).optString("text")+"<br>";
+					//System.out.println(array.getJSONObject(i).optString("text")+"\n");
 				}
-				return json;
+				return output;
 			}
 			
 		} catch (ProtocolException e) {
@@ -116,15 +117,46 @@ public class ApiMain extends HttpServlet{
 
 	}
 	
-	public static String apiGetFollower() {
-		
-		
+	public static String apiGetFollower(String name) {
+		try {
+			makeConnection("https://api.twitter.com/1.1/followers/ids.json?screen_name=" + name);
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Authorization", "Bearer" + " " + bearer_key);
+			connection.connect();
+			if(connection.getResponseMessage().equals("OK")) {
+				JSONObject json = inputStreamToJSON();
+				JSONArray array = json.getJSONArray("ids");
+				String output = "";
+				for(int i = 0; i<15; i++) {
+					System.out.println(array.optString(i) + "\n");
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
 		return null;
 	}
 	
-	public static String apiGetTrends() {
-		
-		
+	public static String apiGetTrends(String id) {
+		try {
+			makeConnection("https://api.twitter.com/1.1/trends/place.json?id=" + id);
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Authorization", "Bearer" + " " + bearer_key);
+			connection.connect();
+			if(connection.getResponseMessage().equals("OK")) {
+				JSONObject json = inputStreamToJSON();
+				JSONArray array = json.getJSONArray("trends");
+				for(int i = 0; i<array.length(); i++) {
+					System.out.println(array.getJSONObject(i).optString("name"));
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
@@ -148,7 +180,9 @@ public class ApiMain extends HttpServlet{
 		return null;
 	}
 	
-	
+	public static String test() {
+		return "TESTING";
+	}
 	
 	
 	public static JSONObject inputStreamToJSON() throws IOException {
